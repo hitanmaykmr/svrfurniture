6edb22b093a824c33de34b0e0fa8e22f5c34e7d4
hitanmaykmr <167610618+hitanmaykmr@users.noreply.github.com>
2024-08-18 10:19:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function toggleImages() {
-  var moreImages = document.getElementById("moreImages");
-  if (moreImages.style.display === "none" || moreImages.style.display === "") {
-      moreImages.style.display = "grid";
+function toggleImages(id) {
+  const images = document.getElementById(id);
+  if (images.style.display === "block") {
+      images.style.display = "none";
   } else {
-      moreImages.style.display = "none";
+      images.style.display = "block";
   }
+}
+
+function closeLightbox() {
+  const lightbox = document.getElementById("lightbox");
+  lightbox.style.display = "none";
 }
